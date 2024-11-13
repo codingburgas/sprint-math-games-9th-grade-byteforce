a24b16dacf5495646d91da6068096724d66b1f47
VMDamyanova23 <VMDamyanova23@codingburgas.bg>
2024-11-13 21:25:48 +0200

    Add edited rules folder

--- rules/rules.cpp
+++ rules/rules.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 int main()
 {
-
-	cout << setw(40) << "+----------+----------+----------+----------+----------+----------+" << endl;
-	cout << setw(40) << "|-----------------------------------------------------------------|" << endl;
-	cout << setw(40) << "|                      How to play the game?                      |" << endl;
-	cout << setw(40) << "|                                                                 |" << endl;
-	cout << setw(40) << "|                 Crossowrd is a word puzzle game.                |" << endl;
-	cout << setw(40) << "|               The rules of the game are very simple.            |" << endl;
-	cout << setw(40) << "|     The words are numbered and there are short definitions      |" << endl;
-	cout << setw(40) << "|  corresponding to the number of the words. You have to fill in  |" << endl;
-	cout << setw(40) << "|  the word by placing one letter in each square of the crossowrd |" << endl;
-	cout << setw(40) << "|  You have to know to know the definiion of the words in order   |" << endl;
-	cout << setw(40) << "|     to complete the game. Each level gets harder and harder.    |" << endl;
-	cout << setw(40) << "|                                                                 |" << endl;
-	cout << setw(40) << "|                      Good luck and have fun!                    |" << endl;
-	cout << setw(40) << "|-----------------------------------------------------------------|" << endl;
-	cout << setw(40) << "+----------+----------+----------+----------+----------+----------+" << endl;
+	const string LIGHTAQUA = "\033[38;2;173;216;230m";
+	cout << setw(40) << LIGHTAQUA << "+----------+----------+----------+----------+----------+----------+" << endl;
+	cout << setw(40) << LIGHTAQUA << "|-----------------------------------------------------------------|" << endl;
+	cout << setw(40) << LIGHTAQUA << "|                      How to play the game?                      |" << endl;
+	cout << setw(40) << LIGHTAQUA << "|                                                                 |" << endl;
+	cout << setw(40) << LIGHTAQUA << "|                 Crossowrd is a word puzzle game.                |" << endl;
+	cout << setw(40) << LIGHTAQUA << "|               The rules of the game are very simple.            |" << endl;
+	cout << setw(40) << LIGHTAQUA << "|      The words are numbered and there are short definitions     | " << endl;
+	cout << setw(40) << LIGHTAQUA << "|  corresponding to the number of the words. You have to fill in  |" << endl;
+	cout << setw(40) << LIGHTAQUA << "|  the word by placing one letter in each square of the crossowrd |" << endl;
+	cout << setw(40) << LIGHTAQUA << "|  You have to know to know the definiion of the words in order   |" << endl;
+	cout << setw(40) << LIGHTAQUA << "|     to complete the game. Each level gets harder and harder.    |" << endl;
+	cout << setw(40) << LIGHTAQUA << "|                                                                 |" << endl;
+	cout << setw(40) << LIGHTAQUA << "|                      Good luck and have fun!                    |" << endl;
+	cout << setw(40) << LIGHTAQUA << "|-----------------------------------------------------------------|" << endl;
+	cout << setw(40) << LIGHTAQUA << "+----------+----------+----------+----------+----------+----------+" << endl;
 
 	cout << endl;
 	cout << "<--Back" << endl;
