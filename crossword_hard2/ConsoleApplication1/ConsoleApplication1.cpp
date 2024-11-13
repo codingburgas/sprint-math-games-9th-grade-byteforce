2fec57dca192700aba485c17e2880bbd446bb99d
danailpetrov1 <danail.z.petrov@gmail.com>
2024-11-13 21:16:12 +0200

    add crossword_hard2 folder

---
+++ crossword_hard2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,222 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const string LIGHTAQUA = "\033[38;2;173;216;230m";
+// Words and clues for the crossword puzzle
+struct CrosswordEntry {
+    string answer;
+    string clue;
+    int number;
+};
+
+// Initialize the crossword puzzle with answers and clues
+vector<CrosswordEntry> crossword = {
+    { "ANALYZE", "To examine data in detail to draw conclusions.", 1 },
+    { "GUIDELINES", "Detailed instructions for completing a task.", 2 },
+    { "COMMAND", "An instuction given to a computer.", 3 },
+    { "REPAIR", "To fix or correct errors in a system.", 4 },
+    { "WORDSEARCH", "A puzzle involving hidden words in a grid.", 5 },
+    { "MYSTIC", "Something mysterious or difficult to understand.", 6 },
+    { "ANSWER", "A solution or response to a question.", 7 }
+};
+
+// Function to display the crossword layout
+void displayCrossword() {
+    cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+\n";
+    cout << LIGHTAQUA << " 1      |   |   |   |   |   |   |   |\n";
+    cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+---+---+---+---+\n";
+    cout << LIGHTAQUA << " 2          |   |   |   |   |   |   |   |   |   |   |\n";
+    cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+    cout << LIGHTAQUA << " 3              |   |   |   |   |   |   |   |\n";
+    cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+    cout << LIGHTAQUA << " 4                  |   |   |   |   |   |   |\n";
+    cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+    cout << LIGHTAQUA << " 5          |   |   |   |   |   |   |   |   |   |   |\n";
+    cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+    cout << LIGHTAQUA << " 6              |   |   |   |   |   |   |\n";
+    cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+    cout << LIGHTAQUA << " 7                  |   |   |   |   |   |   |\n";
+    cout << LIGHTAQUA << "                    +---+---+---+---+---+---+\n";
+}
+
+// Function to display clues for the crossword puzzle
+void displayClues() {
+    cout << "\nClues:\n";
+    for (const auto& entry : crossword) {
+        cout << entry.number << ". " << entry.clue << "\n";
+    }
+}
+
+// Function to check if a guessed word is correct
+bool checkGuess(int clueNumber, const std::string& guess) {
+    for (const auto& entry : crossword) {
+        if (entry.number == clueNumber) {
+            string upperGuess = guess;
+            transform(upperGuess.begin(), upperGuess.end(), upperGuess.begin(), ::toupper); // Convert guess to uppercase
+            return upperGuess == entry.answer;
+        }
+    }
+    return false;
+}
+
+// Function to update the crossword layout with the correct answer
+void updateCrossword(int clueNumber, const std::string& answer) {
+    // Replace the '|' characters with the letters of the answer in the appropriate row
+    if (clueNumber == 1) {
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 1      | " << answer[0] << " | " << answer[1] << " | " << answer[2] << " | " << answer[3] << " | " << answer[4] << " | " << answer[5] << " | " << answer[6] << " | " << "\n";
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 2          |   |   |   |   |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 3              |   |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 4                  |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 5          |   |   |   |   |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 6              |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 7                  |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                    +---+---+---+---+---+---+\n";
+    }
+    else if (clueNumber == 2) {
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 1      | A | N | A | L | Y | Z | E |\n";
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 2          | " << answer[0] << " | " << answer[1] << " | " << answer[2] << " | " << answer[3] << " | " << answer[4] << " | " << answer[5] << " | " << answer[6] << " | " << answer[7] << " | " << answer[8] << " | " << answer[9] << " | " << answer[10] << "\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 3              |   |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 4                  |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 5          |   |   |   |   |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 6              |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 7                  |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                    +---+---+---+---+---+---+\n";
+    }
+    else if (clueNumber == 3) {
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 1      | A | N | A | L | Y | Z | E |\n";
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 2          | G | U | I | D | E | L | I | N | E | S |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 3              | " << answer[0] << " | " << answer[1] << " | " << answer[2] << " | " << answer[3] << " | " << answer[4] << " | " << answer[5] << " | " << answer[6] << " |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 4                  |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 5          |   |   |   |   |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 6              |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 7                  |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                    +---+---+---+---+---+---+\n";
+    }
+    else if (clueNumber == 4) {
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 1      | A | N | A | L | Y | Z | E |\n";
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 2          | G | U | I | D | E | L | I | N | E | S |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 3              | C | O | M | M | A | N | D |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 4                  | " << answer[0] << " | " << answer[1] << " | " << answer[2] << " | " << answer[3] << " | " << answer[4] << " | " << answer[5] << " | \n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 5          |   |   |   |   |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 6              |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 7                  |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                    +---+---+---+---+---+---+\n";
+    }
+    else if (clueNumber == 5) {
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 1      | A | N | A | L | Y | Z | E |\n";
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 2          | G | U | I | D | E | L | I | N | E | S |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 3              | C | O | M | M | A | N | D |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 4                  | R | E | P | A | I | R |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 5          | " << answer[0] << " | " << answer[1] << " | " << answer[2] << " | " << answer[3] << " | " << answer[4] << " | " << answer[5] << " | " << answer[6] << " | " << answer[7] << " | " << answer[8] << " | " << answer[9] << " |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 6              |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 7                  |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                    +---+---+---+---+---+---+\n";
+    }
+    else if (clueNumber == 6) {
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 1      | A | N | A | L | Y | Z | E |\n";
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 2          | G | U | I | D | E | L | I | N | E | S |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 3              | C | O | M | M | A | N | D |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 4                  | R | E | P | A | I | R |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 5          | W | O | R | D | S | E | A | R | C | H |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 6              | " << answer[0] << " | " << answer[1] << " | " << answer[2] << " | " << answer[3] << " | " << answer[4] << " | " << answer[5] << " |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 7                  |   |   |   |   |   |   |\n";
+        cout << LIGHTAQUA << "                    +---+---+---+---+---+---+\n";
+    }
+    else if (clueNumber == 7) {
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 1      | H | A | B | I | T | A | T |\n";
+        cout << LIGHTAQUA << "        +---+---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 2          | Q | U | A | D | R | A | N | G | L | E |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 3              | N | E | G | L | E | C | T |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 4                  | D | E | B | U | G |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 5          | W | O | R | D | S | E | A | R | C | H |\n";
+        cout << LIGHTAQUA << "            +---+---+---+---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 6              | M | Y | S | T | I | C |\n";
+        cout << LIGHTAQUA << "                +---+---+---+---+---+---+---+\n";
+        cout << LIGHTAQUA << " 7                  | " << answer[0] << " | " << answer[1] << " | " << answer[2] << " | " << answer[3] << " | " << answer[4] << " | " << answer[5] << " | \n";
+        cout << LIGHTAQUA << "                    +---+---+---+---+---+---+\n";
+    }
+}
+
+int main() {
+    cout << "Welcome to the Interactive Crossword Puzzle!\n";
+
+    // Display the crossword structure
+    displayCrossword();
+
+    // Display the clues
+    displayClues();
+
+    int correctAnswers = 0;
+    for (const auto& entry : crossword) {
+        string guess;
+        cout << "\nEnter your guess for " << entry.number << " Across: ";
+        getline(std::cin, guess); // Get user's guess
+
+        if (checkGuess(entry.number, guess)) {
+            cout << "Correct!\n";
+            correctAnswers++;
+            updateCrossword(entry.number, entry.answer); // Update crossword layout
+        }
+        else {
+            cout << "Incorrect. The correct answer was: " << entry.answer << "\n";
+        }
+    }
+
+    // Display final score
+    cout << "\nYou got " << correctAnswers << " out of " << crossword.size() << " correct!\n";
+    if (correctAnswers == crossword.size()) {
+        cout << "Congratulations! You completed the crossword!\n";
+    }
+    else {
+        cout << "Better luck next time!\n";
+    }
+}
